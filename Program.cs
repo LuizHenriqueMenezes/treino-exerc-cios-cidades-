b834f3450d7a34d36c2c23c3f1b136571f123e18
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-04 10:40:04 -0300

    Fim do treino

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace treino_exerc_cios_cidades_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Peruíbe");
+            Console.WriteLine("Itanhaém");
+             Console.WriteLine("Praia Grande");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Mongaguá");
+  
+
+        }
+    }
+}
